54c776eb9a43c0c86d1df2c0a663687cc07775c3
Michael Zijlstra <mzijlstra@gmail.com>
2013-05-27 06:50:51 -0500

    refactored solution 31

--- prob031/prob31.py
+++ prob031/prob31.py
@@ -1,17 +1,16 @@
+# Euler Project
+# Solution 31
+# Michael Zijlstra
+# Updated version 05/27/2013
 
 coins = [1, 2, 5, 10, 20, 50, 100, 200]
 
 def findPerms(rem, idx):
 	if rem == 0:
 		return 1
-	elif idx == 0:
-		return findPerms(rem - coins[idx], idx)
-	else:
-		if rem >= coins[idx]:
-			return findPerms(rem, idx - 1) + findPerms(rem - coins[idx], idx) 
-		else:
-			return findPerms(rem, idx - 1)
-
-
+	if rem < 0 or idx < 0:
+		return 0
+	if rem > 0:
+		return findPerms(rem - coins[idx], idx) + findPerms(rem, idx -1)
 
 print findPerms(200, 7)
